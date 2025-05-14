81aef2b150cf254da60d8056b1eebf93d2c3a1fe
deepakdroi <deepak.mahto706@gmail.com>
2025-05-15 00:53:00 +0530

    fix 03

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -148,7 +148,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
