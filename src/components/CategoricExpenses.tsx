cf5ab41569a305eb3f5869c43274f71177ef66ec
deepakdroi <deepak.mahto706@gmail.com>
2025-06-24 19:56:20 +0530

    feat: enhance PieChart with dummy data rendering and visual improvements

--- src/components/CategoricExpenses.tsx
+++ src/components/CategoricExpenses.tsx
@@ -10,13 +10,49 @@ import {
 import { TrendingUp } from "lucide-react";
 import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip } from "recharts";
 
+interface LabelProps {
+  cx: number;
+  cy: number;
+  midAngle: number;
+  innerRadius: number;
+  outerRadius: number;
+  percent: number;
+}
+
 const data = [
   { name: "Group A", value: 400 },
   { name: "Group B", value: 300 },
   { name: "Group C", value: 300 },
   { name: "Group D", value: 200 },
 ];
+
 const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+}: LabelProps) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
 
 export default function CategoricExpense() {
   return (
@@ -27,34 +63,15 @@ export default function CategoricExpense() {
       </CardHeader>
       <CardContent className="min-h-[300px]">
         <ResponsiveContainer width="100%" height={300}>
-          <PieChart width={800} height={400}>
+          <PieChart width={400} height={400}>
             <Pie
               data={data}
-              cx={120}
-              cy={200}
-              innerRadius={60}
+              cx="50%"
+              cy="50%"
+              labelLine={false}
+              label={renderCustomizedLabel}
               outerRadius={80}
               fill="#8884d8"
-              paddingAngle={5}
-              dataKey="value"
-            >
-              {data.map((entry, index) => (
-                <Cell
-                  key={`cell-${index}`}
-                  fill={COLORS[index % COLORS.length]}
-                />
-              ))}
-            </Pie>
-            <Pie
-              data={data}
-              cx={420}
-              cy={200}
-              startAngle={180}
-              endAngle={0}
-              innerRadius={60}
-              outerRadius={80}
-              fill="#8884d8"
-              paddingAngle={5}
               dataKey="value"
             >
               {data.map((entry, index) => (
